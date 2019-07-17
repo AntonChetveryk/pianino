5d0556778772997e4206a39c259bd0e6aa05da61
AntonChetveryk <chetverykanton92@gmail.com>
2019-07-17 12:13:02 +0300

    click

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+////event "клавиатуры"
+const div = document.querySelector('.test');
 const item = document.querySelectorAll('.item'),
+	container = document.querySelector('.container'),
 	audio = document.querySelectorAll('audio'),
 	listnerKeyDown = function (event) {
 		const keyCode = event.keyCode;
@@ -16,10 +19,22 @@ const item = document.querySelectorAll('.item'),
 			if (keyCode == item[i].id) {
 				item[i].classList.remove('bigger');
 			}
-			
+
 		}
 	};
-
 document.body.addEventListener('keydown', listnerKeyDown);
 
 document.body.addEventListener('keyup', listnerKeyUp);
+
+
+
+/// event "мышь"
+function clickMouse(event) {
+	if (event.target.getAttribute('Class') === 'item') {
+		console.log(event.target.getAttribute('Class'));
+		event.target.childNodes[1].currentTime = 0;
+		event.target.childNodes[1].play();
+	}
+}
+
+container.addEventListener('click', clickMouse);
